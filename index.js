24a6cfdb8a7c486e98c488a9df98227ab6df3826
Harpreet Ghotra <harpreetghotra40@icloud.com>
2019-07-14 16:25:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(numValue){
   // Write your code here!
+  if (numValue > 2500){
+    return 'No can do.';
+  }else if (numValue > 2000){
+    return 'I will gladly take your thirty bucks.';
+  }else {
+    return "This one is on me!";
+  }
+  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city === "NYC"){
+    return "Ok, sounds good.";
+  }
+  return "No go."
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === 'generous'){
+    return 'Thank you so much.';
+  }else if (tip === 'not as generous'){
+    return 'Thank you.';
+  }else {
+    return "Bye.";
+  }
 }
