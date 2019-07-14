b66e780e2a617e1206b8eb6057c0362f2935e764
TimeSmash <TimeSmash@github.com>
2019-07-13 20:03:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride <= 400) {
+    return "This one is on me!"
+  } else if (ride >2000 && ride <2500) {
+    return "I will gladly take your thirty bucks."
+  } if (ride > 2500) {
+    return "No can do."
+  }
+}
+function ternaryCheckCity(city){
+let response = (city == "NYC") ? "Ok, sounds good." : "No go."
+return response
+  // switch (city){
+  //   case "NYC":
+  //     return "Ok, sounds good.";
+  //     break;
+  //   default:
+  //     return "No go."
+  //     break;
+  // }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//Or use ternary operator like so:
+// case === "NYC" ? "Ok, sounds good."
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye.";
+      break;
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
