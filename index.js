fe3dcf3851619798301460544844f2f32499ff1e
te68 <te68@cornell.edu>
2019-07-15 09:47:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet > 2500)
+    {return 'No can do.'}
+  else if (feet > 2000)
+  {return 'I will gladly take your thirty bucks.'}
+  else if (feet <= 400)
+    {return 'This one is on me!';}
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let answer
+  (city === "NYC") ? (answer = "Ok, sounds good.") : (answer = "No go.")
+  return answer
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    case 'thanks for everything':
+    return "Bye."
+  }
 }
